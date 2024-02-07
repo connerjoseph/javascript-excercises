282e3b78561808e77a5a1abc6f341d1d30326776
Conner Joseph <cradjoseph@gmail.com>
2024-02-07 16:00:25 +0000

    Add prompt

--- Section 14 Intro to JS/jsVariables.js
+++ Section 14 Intro to JS/jsVariables.js
@@ -1,5 +1,9 @@
 prompt("What is your name?");
 
 
-var myName = "Conner"
-alert(myName)
+var myName = "Conner";
+alert(myName);
+
+var myName = "Conner";
+var yourName = prompt ("What is your name?");
+alert ("My name is "  + myName + ", welcome home " + yourName + "!");
