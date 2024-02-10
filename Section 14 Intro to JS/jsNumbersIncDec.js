e90285beda550005fb6e39a8037c621f6608a57f
Conner Joseph <cradjoseph@gmail.com>
2024-02-10 22:28:30 +0000

    Add Increment and Decrement

---
+++ Section 14 Intro to JS/jsNumbersIncDec.js
@@ -0,0 +1,25 @@
+var x = 5;
+x = x + 1; //6
+
+// instead of writing out x = x + 1
+
+
+//Increment 
+var x = 5;
+x++; //6
+
+//Decrement
+
+var x = 5;
+x--;//4
+
+// C++ was increment c + 1
+
+var x = 5;
+
+x += 2; //7   (increase value of x by more than 1)
+
+
+var x = 5;
+var y = 3;
+x += y; // 8
