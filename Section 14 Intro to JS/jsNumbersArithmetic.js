74167860db5d1d37cbb5b9929f986f0a34b27e75
Conner Joseph <cradjoseph@gmail.com>
2024-02-10 22:03:09 +0000

    Add numbers

---
+++ Section 14 Intro to JS/jsNumbersArithmetic.js
@@ -0,0 +1,9 @@
+var cost = 3 + (5 * 2)
+var cost = (3 + 5) * 2
+
+
+
+
+var dogAge = prompt("How old is your dog?");
+var humanAge = ((dogAge - 2) * 4) + 21;
+alert("Your dog is " + humanAge + " years old in human years.");
