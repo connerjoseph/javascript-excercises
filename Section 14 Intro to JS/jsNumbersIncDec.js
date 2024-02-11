f5644d146b92b6dd27361a4855f8ea327f885864
Conner Joseph <cradjoseph@gmail.com>
2024-02-11 00:31:09 +0000

    Add number challenge

--- Section 14 Intro to JS/jsNumbersIncDec.js
+++ Section 14 Intro to JS/jsNumbersIncDec.js
@@ -23,3 +23,15 @@ x += 2; //7   (increase value of x by more than 1)
 var x = 5;
 var y = 3;
 x += y; // 8
+
+
+var num = 12;
+num /= 3; // = 4
+
+var a = 16 % 4; // = 0
+
+
+
+var x = 3;
+var y = x++;
+y += 1 // y = 4
