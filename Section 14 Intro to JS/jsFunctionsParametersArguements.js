98bca66dcf538a3a98df036798b5db3a4ddbb111
Conner Joseph <cradjoseph@gmail.com>
2024-02-21 10:25:47 +0000

    Add Parameters and Arguements

--- Section 14 Intro to JS/jsFunctionsParametersArguements.js
+++ Section 14 Intro to JS/jsFunctionsParametersArguements.js
@@ -1,8 +1,7 @@
 // creating the function
 
 function getMilk(bottles) {
-
-var cost = bottles * 1.5;
+  var cost = bottles * 1.5;
 }
 // do something with cost
 
@@ -10,42 +9,58 @@ getMilk(2);
 
 //calling the function ^
 
-
 function getMilk(bottles) {
-
-    console.log("moveUp");
-    console.log("moveDown");
-    console.log("moveLeft");
-    console.log("move");
-    console.log("buy" + bottles + "bottles of milk")
-
+  console.log("moveUp");
+  console.log("moveDown");
+  console.log("moveLeft");
+  console.log("move");
+  console.log("buy" + bottles + "bottles of milk");
 }
 
 getMilk(12);
 
-
-// cost of 
-
+// cost of
 
 function getMilk(money) {
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveUp");
 
-console.log("leaveHouse");
-console.log("moveRight");
-console.log("moveUp");
-console.log("moveRight");
-console.log("moveUp");
+  var numberOfBottles = money / 1.5;
 
-var numberOfBottles = money / 1.5
+  console.log("buy " + numberOfBottles + " bottles of milk");
 
-console.log ("buy " + numberOfBottles + " bottles of milk");
-
-
-console.log("moveRight");
-console.log("moveUp");
-console.log("moveRight");
-console.log("moveUp");
-
-
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveUp");
 }
 
 getMilk(5);
+
+
+// Rounding to whole number, must go down because you can't round up to the quanitity of items you buy
+
+function getMilk(money) {
+  console.log("leaveHouse");
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveUp");
+
+  var numberOfBottles = Math.floor(money / 1.5);
+
+  console.log("buy " + numberOfBottles + " bottles of milk");
+
+  console.log("moveRight");
+  console.log("moveUp");
+  console.log("moveRight");
+  console.log("moveUp");
+}
+
+getMilk(5);
+
+
+/// let x = Math.floor(1.6)
