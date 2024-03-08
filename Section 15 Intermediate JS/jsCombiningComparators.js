8f44889952eea428a2fa0731cf9a07a4177494a7
Conner Joseph <cradjoseph@gmail.com>
2024-03-08 12:43:09 +0000

    Add basic comparators

--- Section 15 Intermediate JS/jsCombiningComparators.js
+++ Section 15 Intermediate JS/jsCombiningComparators.js
@@ -1,2 +1,25 @@
-// && 
-//
+// && AND
+// || OR
+// ! NOT
+// 3 Basic comparators
+
+
+prompt("What is your name?");
+prompt("What is their name?");
+
+var loveScore = Math.random() * 100;
+loveScore = Math.floor(loveScore) + 1;
+
+if (loveScore > 70){
+    alert("Your love score is " + loveScore + "% " + "You love eachother like Kanye loves Kanye.");
+}
+
+if (loveScore > 30 && loveScore <= 70) {
+    alert("Your love score is " + loveScore + "%");
+
+}
+
+if (loveScore <= 30) {
+    alert("Your love score is " + loveScore + "%" + " Rethink this one...");
+}
+
