0af1f66f167ebda046fdd6adf9e543741d6ba57d
Conner Joseph <cradjoseph@gmail.com>
2024-03-21 16:41:52 +0000

    Add notes of all attributes changed

--- Mentorship task/jsSpeechValue.js
+++ Mentorship task/jsSpeechValue.js
@@ -28,3 +28,59 @@ var rate = document.getElementById('rate-option').value;
 // https://8d5e-2a09-bac0-1000-cc5-00-17a-71.ngrok-free.app/
 
 // https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
+
+// Below is lesson in real form
+
+function persistChange(elm) {
+    const elm_type = elm.type.toLowerCase();
+    let value;
+  
+    if (elm_type === 'checkbox' )  {
+      value = elm.checked;
+    } else if (elm_type == 'select-one') {
+      value = elm.selectedIndex;
+    } else {
+      value = elm.value;
+    }
+  
+    localStorage.setItem(elm.id, value);
+  }
+  
+  function loadPersistedOption(id) {
+    const elm = document.getElementById(id);
+    const persisted_val = localStorage.getItem(id);
+  
+    if (!elm) {
+      return;
+    }
+  
+    if (persisted_val === null) {
+      return;
+    }
+  
+    let elm_type = elm.type.toLowerCase();
+    if (elm_type === 'checkbox' )  {
+      elm.checked = persisted_val === 'true';
+    } else if (elm_type == 'select-one') {
+      elm.selectedIndex = persisted_val;
+    } else {
+      elm.value = persisted_val;
+    }
+  }
+  
+  function loadPersistedSpeechOptions() {
+    loadPersistedOption('rate-option');
+    loadPersistedOption('volume-option');
+    loadPersistedOption('pitch-option');
+    loadPersistedOption('speech-option');
+    loadPersistedOption('voice-option');
+  }
+  
+  window.addEventListener('load', (event) => {
+    // console.log('-t- load');
+    loadPersistedSpeechOptions();
+  });
+  
+  document.addEventListener('change', (event) => {
+    persistChange(event.target);
+  });
