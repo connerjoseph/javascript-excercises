9b65c26839e192bd1fccf2bc1e5d5ab35a9c1c81
Conner Joseph <cradjoseph@gmail.com>
2024-07-15 18:53:46 +0100

    Add debugger

--- DOM - Drum Kit/index.js
+++ DOM - Drum Kit/index.js
@@ -11,7 +11,7 @@ for (var i = 0; i<numberOfDrumButtons; i++) {
 
 }
 
-
+// pass in a function as an input to another function
 
 function calculator(num1, num2) {
     return num1 + num2;
@@ -29,3 +29,29 @@ function calculator(num1, num2) {
 
 calculator(10, 3);
 30
+
+
+// input function into another function.
+
+function  add(num1, num2) {
+    return num1 + num2;
+}
+function  multiply(num1, num2) {
+    return num1 * num2;
+}
+function calculator(num1, num2,operator) {
+    return operator(num1, num2);
+}
+
+//
+calculator(4,5, add);
+9
+calculator(4,5, multiply);
+20
+
+// debugger
+
+debugger;
+calculator(3,4, multiply);
+
+// for confusion when you don't know what function is doing
