c92b725462bf41eae1998859478821f32deda044
Conner Joseph <cradjoseph@gmail.com>
2024-11-21 23:40:24 +0000

    Add drum audio

---
+++ DOM - Drum Kit/functionCalcultors.js
@@ -0,0 +1,92 @@
+
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i<numberOfDrumButtons; i++) {
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        
+        console.log(this.innerHTML);
+    });
+
+}
+
+
+// var audio = new Audio("sounds/tom-1.mp3");
+//         audio.play();
+//
+//
+
+// changing text of button clicked using identity 
+console.log(this.innerHTML); --> this.StylePropertyMap.color = "white";
+
+
+
+
+
+// var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+// for (var i = 0; i<numberOfDrumButtons; i++) {
+
+    // document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+        // alert("I got clicked!");
+    
+        // what to do when click detected.
+    //});
+
+//}
+
+
+
+// pass in a function as an input to another function
+
+
+function calculator(num1, num2) {
+    return num1 + num2;
+}
+
+calculator(4,5);
+9
+
+
+
+function calculator(num1, num2) {
+    return num1 * num2;
+}
+
+
+calculator(10, 3);
+30
+
+
+// input function into another function.
+
+function  add(num1, num2) {
+    return num1 + num2;
+}
+function  multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function  subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function  divide(num1, num2) {
+    return num1 / num2;
+}
+function calculator(num1, num2,operator) {
+    return operator(num1, num2);
+}
+
+//
+calculator(4,5, add);
+9
+calculator(4,5, multiply);
+20
+
+// debugger
+
+debugger;
+calculator(3,4, multiply);
+
+// for confusion when you don't know what function is doing
