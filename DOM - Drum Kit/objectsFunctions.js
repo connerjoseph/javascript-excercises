a36e5657afff51db0c210c6760916acf8bd77f6d
Conner Joseph <cradjoseph@gmail.com>
2024-11-22 02:41:20 +0000

    Add constructor function

--- DOM - Drum Kit/objectsFunctions.js
+++ DOM - Drum Kit/objectsFunctions.js
@@ -3,3 +3,21 @@ var houseKeeper1 = {
     name: "Connie",
     CleaningRepertoire: ["bathroom", "lobby", "bedroom"]
 }
+
+
+// Constructor Function
+
+function BellBoy (name, age, hasWorkPermit, languages) {
+    this.name = name;
+    this.age = age;
+    this.hasWorkPermit = hasWorkPermit;
+    this.Languages = languages;
+}
+
+// Initialise Object 
+
+var bellBoy = new BellBoy("Bob", 19, true, ["French", "Mandarin"]);
+
+var bellBoy = new BellBoy("John", 30, true, ["French", "Thai"]);
+
+var bellBoy = new BellBoy("Marko", 42, false, ["Spanish", "English"]);
