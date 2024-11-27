c67a661867dbeee2b3ffbd0eb60d99c3088019f6
connerjoseph <cradjoseph@gmail.com>
2024-11-27 17:33:45 +0000

    Add manipulating style

--- jQuery/jQuery.js
+++ jQuery/jQuery.js
@@ -1,3 +1,5 @@
+// SELECTING ELEMENTS
+
 document.querySelector("h1");
 // jQuery
 $("h1");
@@ -5,3 +7,16 @@ $("h1");
 document.querySelectorAll("button");
 // jQuery
 $("button");
+
+
+// MANIPULATING STYLES
+
+$("h1").css("color");
+console.log($("h1").css("color"));
+
+console.log($("h1").css("font-size"));
+
+
+// single property you are getting the value, if you have have two your setting the value
+
+$("h1").css("font-size", "5rem");
