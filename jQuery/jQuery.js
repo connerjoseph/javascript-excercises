709964095b565fcc5b19c1ea87c9b644d30297ab
connerjoseph <cradjoseph@gmail.com>
2024-11-27 17:39:09 +0000

    Add class manipulation

--- jQuery/jQuery.js
+++ jQuery/jQuery.js
@@ -20,3 +20,11 @@ console.log($("h1").css("font-size"));
 // single property you are getting the value, if you have have two your setting the value
 
 $("h1").css("font-size", "5rem");
+
+// keep javasript about behaviour 
+// keep style code about appearance
+// keep html all about content
+
+$("h1").addClass("big-title");
+// in console to remove
+$("h1").removeClass("big-title");
