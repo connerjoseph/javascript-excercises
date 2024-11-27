a65e433de4cd4c6df171ba7953e048bf841280af
connerjoseph <cradjoseph@gmail.com>
2024-11-27 17:13:16 +0000

    Add Selecting Elements

--- DOM - Drum Kit/index.js
+++ DOM - Drum Kit/index.js
@@ -44,12 +44,76 @@ for (var i = 0; i<numberOfDrumButtons; i++) {
        
         default: console.log(buttonInnerHTML);
 
+        
+
+
             
        }  
+      // this.style.color = "white";
+
+      
+      this.classList.add("pressed");
+
        
     });
 
+   
 }
+
+document.body.addEventListener("keydown", function (event) {
+console.log("." + event.key);
+
+
+var button = document.querySelector("." + event.key) ;
+    var buttonInnerHTML = button.innerHTML;
+
+
+   switch (buttonInnerHTML) {
+    case "w":
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+
+    case "a":  
+    var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();  
+        break;
+
+        case "s":  
+    var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();  
+        break;
+
+        case "d":  
+    var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();  
+        break;
+
+        case "j":  
+    var snare = new Audio("sounds/snare.mp3");
+        snare.play();  
+        break;
+
+        case "k":  
+    var crash = new Audio("sounds/crash.mp3");
+        crash.play();  
+        break;
+
+        case "l":  
+    var kick = new Audio("sounds/kick-bass.mp3");
+        kick.play();  
+        break;
+   
+    default: console.log(buttonInnerHTML);
+        
+   }  
+  // this.style.color = "white";
+
+  
+  button.classList.add("pressed");
+
+   
+});
 
 
   
---
+++ jQuery/jQuery.js
@@ -0,0 +1,7 @@
+document.querySelector("h1");
+// jQuery
+$("h1");
+
+document.querySelectorAll("button");
+// jQuery
+$("button");
