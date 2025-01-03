a8d6a7473b60a86b8ec0b9604ae869f946c6bc01
connerjoseph <cradjoseph@gmail.com>
2025-01-03 08:40:36 +0000

    Add comment story

--- Auditorium - JS/comment.js
+++ Auditorium - JS/comment.js
@@ -13,3 +13,53 @@ console.log("This is code");
 I can write anything in between
 the start and the end.
 */
+
+
+//Comment out some lines of code to print out ART.
+// console.log("J");
+console.log("A");
+/*
+console.log("V");
+console.log("A");
+console.log("S");
+console.log("C");
+*/
+console.log("R");
+// console.log("I");
+// console.log("P");
+console.log("T");
+
+
+
+// Introduction
+console.log("Hmm... strange weather");
+
+// The main character is introduced
+console.log("A child stands in a field of fireflies.");
+console.log("Her name is Kiki, the code daemon.");
+
+// The quest begins
+
+console.log("Kiki begins her journey trying to to take a picture of the fireflies.");
+console.log ("She is searching for a believed to be extinct firefly.");
+
+
+// Problem arises
+
+console.log("In the darkness the light from the fireflies begins to fade...");
+console.log("Black smoke has drifted accross the field, the floating lights begin to vanish and fall...");
+
+// Problem is solved
+
+console.log("Kiki remembers passing a fire station on the way to the field.");
+console.log("Her feet begin to move like never before struggling to breath she sprints to the station in hopes of alerting someone.");
+
+// Conclusion
+
+console.log("After a milelong sprint, Kiki finally makes it.");
+console.log("Too tired to put words together she manages to alert the fireman. They rush off to deal with the problem.");
+console.log("Upon returning, just before sunrise the smell of burnt grass fills the air. All of a sudden a small light slowly lands on her shoulder.");
+console.log("It is the \"extinct firefly\"");
+
+
+// End of story
