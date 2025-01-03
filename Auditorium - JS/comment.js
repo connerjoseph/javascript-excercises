278ab329555bcb99809608fba6c8c527df4dfa20
connerjoseph <cradjoseph@gmail.com>
2025-01-03 08:23:20 +0000

    Add comment js

---
+++ Auditorium - JS/comment.js
@@ -0,0 +1,15 @@
+# Comment
+
+//This code will print "Hello" to the output.
+
+console.log("Hello")
+
+short cut ctrl + / or command + /
+
+//This is a single-line comment.
+console.log("This is code");
+
+/* This is a multi-line comment.
+I can write anything in between
+the start and the end.
+*/
---
+++ Auditorium - JS/consoleLog.js
@@ -0,0 +1,32 @@
+# Console Log
+
+Copiler turns code into 101010101010
+
+**Action vs. Thing**
+
+Like a recipe code is
+
+Console.log(xxx)
+
+xxx”Hello”
+
+console.log(”Hello”)
+
+The action we want the computer to perform is console.log which is to print on screen the thing
+
+The thing code is to tell the computer what to print ex: “Hello, world!”
+
+The computer knows this text is not code due to syntax highlighting
+
+Ending a Sentence of Code**;**
+
+console.log("Hello, world");
+console.log("Hello again!");
+console.log("It's me!");
+console.log("My name is Conner");
+
+How to include double quotes
+
+'My "String"'
+"My \"String\""
+"My "String""
