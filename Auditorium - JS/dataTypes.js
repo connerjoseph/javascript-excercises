4b805568730f0c05acedfb26948abad34685d8fe
connerjoseph <cradjoseph@gmail.com>
2025-01-03 09:08:12 +0000

    Add data type

---
+++ Auditorium - JS/dataTypes.js
@@ -0,0 +1,24 @@
+# Data types
+
+**The String Data type**
+
+console.log("3" + "2");
+
+"A string starts and ends with a double quote"
+
+It is telling the computer this part should be treated as text even a number 
+
+**The Number Data Type**
+
+console.log("3" + "2"); // 32
+console.log(3 + 2); // 5
+
+**Integers**
+
+//This is a whole number
+42
+
+**Floats**
+
+//This is a decimal number
+3.14159
