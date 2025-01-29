0ec9dbfd925cfe1ae31e0dc88cddc7ae76ad77f8
connerjoseph <cradjoseph@gmail.com>
2025-01-29 14:45:21 +0000

    Add variable

--- Auditorium - JS/variables.js
+++ Auditorium - JS/variables.js
@@ -2,3 +2,11 @@ let dataTypeOfHello = typeof("Hello");
 
 console.log(dataTypeOfHello);
 //string
+
+// creating a variable
+let myAge = 12;
+let myFriendAge = 15;
+
+// using a variable
+let totalAge = myAge + myFriendAge;
+console.log(totalAge);
