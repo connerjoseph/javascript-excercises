fcd319783bc0de7e734dceed3f0dc2d83856eee0
connerjoseph <cradjoseph@gmail.com>
2025-01-29 17:27:30 +0000

    Add rules

--- Auditorium - JS/variables.js
+++ Auditorium - JS/variables.js
@@ -12,5 +12,34 @@ let totalAge = myAge + myFriendAge;
 console.log(totalAge);
 
 // tracking the state
-let result = 2.5 + 3; 
+let result = 0;
+
+result = 2.5 + 3;
+console.log(result);
+// output = 5.5
+
+let result = 0;
+
+result = 2.5 + 3;
+console.log(result);
+
 result = result * 2;
+console.log(result);
+// output = 11
+
+// Names must begin with a letter.
+// Names are case sensitive.
+// Reserved words cannot be used as names.
+// Each word in a name should be camelCased.
+
+let name = "Angela";
+console.log(name);
+
+let pocketMoney = 12;
+console.log(pocketMoney);
+
+let typeOf = "Output";
+console.log(typeOf);
+  
+let myFirstVariable = "abc";
+console.log(myFirstVariable);
